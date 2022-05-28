78a5825a55ffaa8242817a16b23bd115f636fe00
Samovol <samovole@gmail.com>
2022-05-28 12:35:46 +0300

    еще раз

--- main.py
+++ main.py
@@ -22,19 +22,20 @@ def generator() -> dict:
 
 
 def main(amount: int) -> json:
-     '''
-     Функция нацелена на то, чтобы я получил зачет
+    """
+    Функция нацелена на то, чтобы я получил зачет
      :param amount:количество книг, которые хотели бы записать в файл
      :return формирует файл json в котором хранятся сгенерированые нами книги:
-     '''
+    """
     pk = 1
+    list_book = []
     for _ in range(amount):
         dict_book = {"Model": MODEL, "pk": pk, "fields": generator()}
-        file = "Books.json"
-        with open(file, "a", encoding="utf-8") as f:
-            json.dump(dict_book, f, indent=4, ensure_ascii=False)
+        list_book.append(dict_book)
         pk += 1
-
+    file = "Books.json"
+    with open(file, "w", encoding="utf-8") as f:
+        json.dump(list_book, f, indent=4, ensure_ascii=False)
 
 if __name__ == '__main__':
     main(100)
